7a5b61b9a42e39efd6f6362027815a885ff0b22c
Brie <breanna.bunge@airbnb.com>
2019-08-12 12:58:43 -0700

    Fix for TypeError from tsc.js

--- frontend/project-001/FooC.jsx
+++ frontend/project-001/FooC.jsx
@@ -6,7 +6,8 @@ function FooC({ message }) {
 }
 
 FooC.propTypes = {
-  message: PropTypes.string.isRequired,
+  // TypeError: Cannot read property 'declarations' of undefined @ node_modules/typescript/lib/tsc.js:76013
+  // message: PropTypes.string.isRequired,
 };
 
 export default FooC;
